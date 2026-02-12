e16a9b580f5c954ecd504c88252e22a3e07c8a9b
Max Swinkels <max@backspaced.nl>
2026-02-12 22:42:26 +0100

    :zap: Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,7 +101,7 @@ export default function Home() {
         </div>
 
         {/* Projects grid */}
-        <div className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+        <div className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
           {filteredProjects.map((project, i) => (
             <ProjectCard
               key={project.id}
